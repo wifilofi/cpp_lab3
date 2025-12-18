5961fd4fa397050e0df18ef6bf58544477447778
wifilofi <90645177+wifilofi@users.noreply.github.com>
2025-12-19 02:21:55 +0300

    add move swap

---
+++ include/sort_algorithms.h
@@ -0,0 +1,23 @@
+﻿//
+// Created by gfn on 12/19/2025.
+//
+
+#ifndef CPP_LAB3_SORT_ALGORITHMS_H
+#define CPP_LAB3_SORT_ALGORITHMS_H
+#include <algorithm>
+
+
+class sort_algorithms
+{
+};
+
+template<typename T>
+inline void MoveSwap(T &a, T &b)
+{
+    T Tmp = std::move(a);
+    a = std::move(b);
+    b = std::move(Tmp);
+}
+
+
+#endif //CPP_LAB3_SORT_ALGORITHMS_H
