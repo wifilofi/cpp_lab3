a9629455dd13bc6303856e48881c7d901548017c
wifilofi <90645177+wifilofi@users.noreply.github.com>
2025-12-19 02:35:31 +0300

    add insertion sort method

--- include/sort_algorithms.h
+++ include/sort_algorithms.h
@@ -11,13 +11,32 @@ class sort_algorithms
 {
 };
 
-template<typename T>
-inline void MoveSwap(T &a, T &b)
+template <typename T>
+inline void MoveSwap(T& a, T& b)
 {
     T Tmp = std::move(a);
     a = std::move(b);
     b = std::move(Tmp);
 }
+
+template <typename T, typename Compare>
+void InsertionSort(T* first, T* last, Compare comp)
+{
+    if (first >= last) return;
+    for (T* It = first + 1; It < last; ++It)
+    {
+        T Key = std::move(*It);
+        T* J = It;
+        while (J > first && comp(Key, *(J - 1)))
+        {
+            *J = std::move(*(J - 1));
+            --J;
+        }
+        *J = std::move(Key);
+    }
+}
+
+
 
 
 #endif //CPP_LAB3_SORT_ALGORITHMS_H
